211adac134529a9b3cdfe25caf81fc6a3292faf2
Phila10 <aurelien.w-joubert@hotmail.fr>
2022-03-18 23:49:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo/Vygon logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="logo-sipv" src={logo}></img>
       </header>
+      <div className="formulaire">
+        <h1>Machine</h1>
+        <div className="formulaire-component">
+          <label>
+            Code :
+            <input type="text" name="name" />
+          </label>
+          <label>
+            DLU :
+            <input type="text" name="name" />
+          </label>
+          <label>
+            Date :
+            <input type="text" name="name" />
+          </label>
+          <label>
+            Visa :
+            <input type="text" name="name" />
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
