8746cc786ab6fa1dca8019474cdd953dad2e13d3
Phila10 <aurelien.w-joubert@hotmail.fr>
2022-03-24 22:09:28 +0100

    add alt img

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="logo-sipv" src={logo}></img>
+        <img className="logo-sipv" src={logo} alt="yes"></img>
       </header>
       <div>
         <Component />
